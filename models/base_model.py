09938d9799386e6a23bc431488635d5e339ae86b
Salma2486 <saloom_001@hotmail.com>
2024-01-11 07:20:38 -0800

    wsef

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+
+
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    dtime_obj = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, dtime_obj)
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         self.updated_at = datetime.now()
+
     def to_dict(self):
         dictFormat = {}
         for key, val in self.__dict__.items():
@@ -17,7 +30,6 @@ class BaseModel:
             else:
                 dictFormat[key] = val
         return dictFormat
-
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
