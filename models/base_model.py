943304b114eb26660f2388f1123b97c64375eb83
Salma2486 <saloom_001@hotmail.com>
2024-01-13 14:13:28 -0800

    asasas

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,44 @@
-#!/usr/bin/pyhon3
-"""
-Parent class that will inherit
-"""
+#!/usr/bin/python3
+"""Module for BaseModel"""
 import uuid
 from datetime import datetime
 from models import storage
 
 
 class BaseModel:
-    """Defines all common attributes/methods
-    """
+    """Base class for other classes"""
+
     def __init__(self, *args, **kwargs):
-        """initializes all attributes
-        """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
-        else:
+        """Initialize BaseModel"""
+        if kwargs:
             f = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(kwargs[key], f)
+                if key in ('created_at', 'updated_at'):
+                    value = datetime.strptime(value, f)
                 if key != '__class__':
                     setattr(self, key, value)
-
-    def __str__(self):
-        """returns class name, id and attribute dictionary
-        """
-        class_name = "[" + self.__class__.__name__ + "]"
-        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
-        return class_name + " (" + self.id + ") " + str(dct)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
-        """updates last update time
-        """
+        """Update the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """creates a new dictionary, adding a key and returning
-        datemtimes converted to strings
-        """
-        new_dict = {}
-
-        for key, values in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        """Return a dictionary representation of the instance"""
+        dict_format = {}
+        for key, val in self.__dict__.items():
+            if isinstance(val, datetime):
+                dict_format[key] = val.isoformat()
             else:
-                if not values:
-                    pass
-                else:
-                    new_dict[key] = values
-        new_dict['__class__'] = self.__class__.__name__
+                dict_format[key] = val
+        return dict_format
 
-        return new_dict
+    def __str__(self):
+        """Return a string representation of the instance"""
+        cl = self.__class__.__name__
+        return f"[{cl}] ({self.id}) <{self.__dict__}>"
