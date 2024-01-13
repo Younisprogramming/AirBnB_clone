d9a1d669a8601a5a85cd907b12c31b464f7cc828
Salma2486 <saloom_001@hotmail.com>
2024-01-13 09:00:23 -0800

    wororoooook

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-""" yjetd yhwr uhteyd ujsyt uj
- dty jdt yjutd"""
+"""Module for BaseModel"""
 import uuid
 from datetime import datetime
 from models import storage
 
-
 class BaseModel:
-    """wr6y hety huesry twr6 yer6y 
-     rt hyywr hy"""
+    """Base class for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """ ywerh  6ywr4 y65wu w65 u w46ry 4"""
+        """Initialize BaseModel"""
         if kwargs:
             f = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(kwargs[key], f)
+                if key in ('created_at', 'updated_at'):
+                    value = datetime.strptime(value, f)
                 if key != '__class__':
                     setattr(self, key, value)
         else:
@@ -26,21 +23,21 @@ class BaseModel:
             storage.new(self)
 
     def save(self):
-        """ yw4 y6wr 6w5 yu5e6u w5r6yu w56 u"""
+        """Update the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """ 6y4w yw56uy w64r yue5r6yu e56hy u"""
-        dictFormat = {}
+        """Return a dictionary representation of the instance"""
+        dict_format = {}
         for key, val in self.__dict__.items():
             if isinstance(val, datetime):
-                dictFormat[key] = val.isoformat()
+                dict_format[key] = val.isoformat()
             else:
-                dictFormat[key] = val
-        return dictFormat
+                dict_format[key] = val
+        return dict_format
 
     def __str__(self):
-        """ 64w yuw6r uw56u w56u 64 """
+        """Return a string representation of the instance"""
         cl = self.__class__.__name__
-        return "[{}] ({}) {}".format(cl, self.id, self.__dict__)
+        return f"[{cl}] ({self.id}) {self.__dict__}"
