ad53ec100e9ddf17cbe9711589006213c5e34303
Salma2486 <saloom_001@hotmail.com>
2024-01-13 08:50:12 -0800

    wororoooook

--- models/base_model.py
+++ models/base_model.py
@@ -35,16 +35,11 @@ class BaseModel:
         """ e5h hyet6 hye5yh e5t yh"""
         dictFormat = {}
         for key, val in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            if isinstance(val, datetime):
+                dictFormat[key] = val.isoformat()
             else:
-                if not values:
-                    pass
-                else:
-                    new_dict[key] = values
-        new_dict['__class__'] = self.__class__.__name__
-
-        return new_dict
+                dictFormat[key] = val
+                return dictFormat
 
     def __str__(self):
         """ e56 hye56 yh5e hy5e h6e5 6e5h"""
