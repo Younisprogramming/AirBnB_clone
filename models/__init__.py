de8b473b64d16aeeee0c18e6c4ade55d198ab74a
Salma2486 <saloom_001@hotmail.com>
2024-01-14 11:26:14 -0800

    f

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,5 @@
 """ Import modules and packages """
 from models.engine.file_storage import FileStorage
-from models.user import User
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.state import State
-from models.review import Review
-from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
