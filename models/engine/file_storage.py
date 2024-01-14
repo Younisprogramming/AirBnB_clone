413252c164b1a89ff3f3cd37c374f7f82036036a
Salma2486 <saloom_001@hotmail.com>
2024-01-14 04:39:20 -0800

    rere

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,7 +30,9 @@ class FileStorage:
             json.dump(dictObj, jsonFile)
 
     def reload(self):
-        """ Deserializes __objects from t srth srth ysrt he JSON file """
+        """ Deserializes __objects from 
+        t srth srth ysrt he JSON file
+        """
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -39,11 +41,10 @@ class FileStorage:
         from models.state import State
         from models.review import Review
         dct = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'State': State, 'City': City,
-               'Amenity': Amenity, ' Review': Review}
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r') as file:
-                data = json.load(file)
-                for value in data.values():
-                    class_name = value.get('__class__')
-                    self.new(dct[class_name](**value))
+                'City': City, 'Amenity': Amenity, 'State': State,
+                'Review': Review}
+
+        if os.path.exists(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path, 'r') as f:
+                for key, value in json.load(f).items():
+                    self.new(dct[value['__class__']](**value))
