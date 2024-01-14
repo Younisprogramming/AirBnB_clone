3d7ba0cf0669a07ca19f70fc2298e19960780f2f
Salma2486 <saloom_001@hotmail.com>
2024-01-14 11:18:24 -0800

    sssssssssss

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,60 +1,35 @@
 #!/usr/bin/python3
-"""
-FileStorage - Serialization and Deserialization of Objects
-"""
-
+""" re hyw56 ehe5yt5y uh5e7 u6r7 """
 import json
-import os.path
+from models.base_model import BaseModel
+import os
 
 
 class FileStorage:
-    """
-    FileStorage Class
-
-    This class serializes instances to a JSON file
-      and deserializes JSON files to instances.
-
-    Attributes:
-        __file_path (str): The path to the JSON file.
-        __objects (dict): A dictionary that stores serialized objects.
-    """
+    """ storage rsth yrst hyrts """
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Returns:
-            dict: A dictionary containing all serialized objects.
-        """
+        """all func.wr hywsrt hy yh"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        Adds a new serialized object to the dictionary.
-
-        Args:
-            obj (BaseModel): The object to be serialized.
-        """
+        """new func. srth rs hyrs hy"""
         key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """
-        Serializes the objects to the JSON file.
-        sda asd
-        ds sadssaf
-        """
-        obj_dict = {}
-        for key, value in FileStorage.__objects.items():
-            obj_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, "w") as file:
-            json.dump(obj_dict, file)
+        """save func.srth sr wsy hwsrt yh"""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
 
     def reload(self):
-        """
-        Deserializes the JSON file to objects.
-        more talk ksadnj kjsa
+        """ Deserializes __objects from
+        t srth srth ysrt he JSON file
         """
         from models.base_model import BaseModel
         from models.user import User
@@ -63,20 +38,16 @@ class FileStorage:
         from models.amenity import Amenity
         from models.state import State
         from models.review import Review
-
-        Models = {
-            'BaseModel': BaseModel,
-            'User': User,
-            'Amenity': Amenity,
-            'Place': Place,
-            'City': City,
-            'State': State,
-            'Review': Review
-        }
-
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r') as file:
-                data = json.load(file)
-                for value in data.values():
-                    class_name = value.get('__class__')
-                    self.new(Models[class_name](**value))
+        dct = {
+                'BaseModel': BaseModel,
+                'User': User,
+                'Amenity': Amenity,
+                'Place': Place,
+                'City': City,
+                'State': State,
+                'Review': Review
+                }
+        if os.path.exists(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path, 'r') as f:
+                for key, value in json.load(f).items():
+                    self.new(dct[value['__class__']](**value))
