0655fcf96b6121e716f5addaad5cfe80ab8c6b84
Younisprogramming <younistaher12341@gmail.com>
2024-01-14 16:21:07 -0800

    try

--- models/base_model.py
+++ models/base_model.py
@@ -15,13 +15,12 @@ class BaseModel:
         eModel class.
         """
         if kwargs:
+            f = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key != "__class__":
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
                     setattr(self, key, value)
-            self.created_at = datetime.strptime(
-                kwargs.get("created_at"), "%Y-%m-%dT%H:%M:%S.%f")
-            self.updated_at = datetime.strptime(
-                kwargs.get("updated_at"), "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
